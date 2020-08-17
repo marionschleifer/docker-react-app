37818fd11d91af742e1cd459b685da377a47feb8
Marion Schleifer <marion@hasura.io>
2020-08-17 10:44:33 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import docker from './docker-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img src={docker} className="App-logo" alt="logo" />
+        <h1>Docker is amazing!</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.docker.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Docker documentation
         </a>
       </header>
     </div>
